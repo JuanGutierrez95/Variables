8d6bc6f5aa26cf76d5cd622404163fd9107bdfc2
ale9516 <juan.gutierrez19958@gmail.com>
2022-03-23 19:54:55 -0300

    first commit

---
+++ variables1.js
@@ -0,0 +1,29 @@
+//Una variable podemos entender como es la representaicon de algun lugar, el memoria que nosotros vamos a reservar para poder guardar un valor y ese valor como vimos puede tener diferentes tipos.
+
+var nombre = "Juan"; //Var es esta representacion del lugar donde vamos a guardar valores si queremos generar un valor de nombre
+
+
+//Declarar una variable: Generamos la palabra reservada var y le ponemos un nombre a ese espacio en la memoria. ejemplo edad
+
+var edad;
+
+//Inicializar esa variable
+edad = 30;
+
+
+
+
+
+//arrays
+//Estamos declarando e inicializando una variable
+var elementos = ["Computadora", "Celular"];
+// = cuando estamos utilizando igual, es un operador que vamos a utilizar en javascript para poder asignar un valor. 
+//Vamos a ver diferentes tipo de operadores, pero es importante que cuando estemos trabajando con una variable si queremos darle un valor, si queremos inicializar esa variable, utilicemos el simbolo de igual. Solo un simbolo de igual que referente a asignar un valor. 
+
+
+//Objeto
+var persona = {
+  nombre: "Juan",
+  edad: 25,
+}
+
